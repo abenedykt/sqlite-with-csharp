41407ea3c656e098e5ffbe45a8e848a6dec38dc1
abenedykt <arkadiusz@benedykt.net>
2014-05-09 22:24:07 +0200

    switching to other orm

--- SyncDb/Program.cs
+++ SyncDb/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Data.SQLite;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
 
 namespace SyncDb
 {
@@ -7,38 +8,68 @@ namespace SyncDb
 	{
 		static void Main(string[] args)
 		{
-			var storage = new LocalStorage(@"Data Source=files.db;Version=3;");
-			storage.AddFile("Hello world");
+			var connectionString = "files.db";
+			var dbFactory = new OrmLiteConnectionFactory(connectionString, SqliteDialect.Provider, false);
+			
+				// Wrap all code in using statement to not forget about using db.Close()
+			using (var db = dbFactory.Open())
+			{
+				db.CreateTableIfNotExists<Note>();
+				db.Insert(new Note
+				{
+					Name = "Hello",
+					Path = "World"
+				});
+
+				var notes = db.Where<Note>("Name", "Hello");
+				foreach (var note in notes)
+				{
+					Console.WriteLine(note.Name);
+				}
+			}
+
 		}
 
 	}
 
-	public class LocalStorage
+		
+
+		class Note
 	{
-		private readonly string _fileName;
-		private SQLiteConnection _connection;
+		[AutoIncrement]
+		// Creates Auto primary key
+		public int Id { get; set; }
 
-		public LocalStorage(string fileName)
-		{
-			_fileName = fileName;
-			_connection = new SQLiteConnection(_fileName);
-			_connection.Open();
-			var newDatabase = _connection.CreateCommand();
-			newDatabase.CommandText = "create table if not exists images (date text, file text, path text, sent integer)";
-			newDatabase.ExecuteNonQuery();
-			Console.WriteLine("created");
-
-		}
-
-		public void AddFile(string fileName)
-		{
-
-
-			var command = _connection.CreateCommand();
-			command.CommandText = string.Format(@"INSERT INTO images values (""{0}"",""{1}"",""{2}"",{3})", fileName, fileName, fileName, 0);
-			command.ExecuteNonQuery();
-			Console.WriteLine("inserted");
-		}
+		public string Name { get; set; }
+		public string Path { get; set; }
 	}
+
+//	public class LocalStorage
+//	{
+//		private readonly string _fileName;
+//		private SQLiteConnection _connection;
+//
+//		public LocalStorage(string fileName)
+//		{
+//			_fileName = fileName;
+//			_connection = new SQLiteConnection(_fileName);
+//			_connection.Open();
+//			var newDatabase = _connection.CreateCommand();
+//			newDatabase.CommandText = "create table if not exists images (date text, file text, path text, sent integer)";
+//			newDatabase.ExecuteNonQuery();
+//			Console.WriteLine("created");
+//
+//		}
+//
+//		public void AddFile(string fileName)
+//		{
+//
+//
+//			var command = _connection.CreateCommand();
+//			command.CommandText = string.Format(@"INSERT INTO images values (""{0}"",""{1}"",""{2}"",{3})", fileName, fileName, fileName, 0);
+//			command.ExecuteNonQuery();
+//			Console.WriteLine("inserted");
+//		}
+//	}
 
 }
